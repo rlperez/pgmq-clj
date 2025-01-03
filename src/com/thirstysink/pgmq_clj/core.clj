d2db991a80d91f77f43a7cc80569826e3f983d92
Rigoberto L. Perez <rlperez@kablamo.me>
2025-01-02 22:26:19 -0500

    ♻️ refactor: add an interface for single operations

--- src/com/thirstysink/pgmq_clj/core.clj
+++ src/com/thirstysink/pgmq_clj/core.clj
@@ -1,1 +1,15 @@
 (ns com.thirstysink.pgmq-clj.core)
+
+(defn create-queue [adapter queue-name] nil)
+
+(defn drop-queue [adapter queue-name] nil)
+
+(defn send-message [adapter queue-name payload] nil)
+
+(defn read-message [adapter queue-name] nil)
+
+(defn pop-message [adapter queue-name] nil)
+
+(defn delete-message [adapter queue-name msg-id] nil)
+
+(defn archive-message [adapter queue-name msg-id] nil)
