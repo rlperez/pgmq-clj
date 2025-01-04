dd62ba05a8d67893f877dac2fea0f671215f40a0
Rigoberto L. Perez <rlperez@kablamo.me>
2025-01-04 14:11:33 -0500

    ♻️ refactor: wrap next.db exceptions with custom exceptions

--- src/com/thirstysink/pgmq_clj/db/adapters/hikari_adapter.clj
+++ src/com/thirstysink/pgmq_clj/db/adapters/hikari_adapter.clj
@@ -8,30 +8,58 @@
   adapter/Adapter
 
   (execute! [this sql params]
-    (jdbc/execute! (:datasource this)
-                   (if (seq params)
-                     [sql params]
-                     [sql])))
+    (try
+      (jdbc/execute! (:datasource this)
+                     (cond-> sql
+                       (seq params) (jdbc/with-parameters params)))
+      (catch Exception e
+        (throw (ex-info "Failed to execute SQL statement"
+                        {:type ::execute-error
+                         :sql sql
+                         :params params}
+                        e)))))
 
   (query [this sql params]
-    (jdbc/execute! (:datasource this)
-                   (if (seq params)
-                     [sql params]
-                     [sql])
-                   {:builder-fn rs/as-unqualified-lower-maps}))
+    (try
+      (jdbc/execute! (:datasource this)
+                     (cond-> sql
+                       (seq params) (jdbc/with-parameters params))
+                     {:builder-fn rs/as-unqualified-lower-maps})
+      (catch Exception e
+        (throw (ex-info "Failed to execute query"
+                        {:type ::query-error
+                         :sql sql
+                         :params params}
+                        e)))))
 
   (with-transaction [this f]
-    (jdbc/with-transaction [tx (:datasource this)]
-      (f (assoc this :datasource tx))))
+    (try
+      (jdbc/with-transaction [tx (:datasource this)]
+        (f (assoc this :datasource tx)))
+      (catch Exception e
+        (throw (ex-info "Failed to execute within a transaction"
+                        {:type ::transaction-error}
+                        e)))))
 
   (close [this]
-    (.close (:datasource this))))
+    (try
+      (.close (:datasource this))
+      (catch Exception e
+        (throw (ex-info "Failed to close the datasource"
+                        {:type ::close-error}
+                        e))))))
 
 (defn make-hikari-adapter [config]
-  (let [datasource (doto (HikariDataSource.)
-                     (.setJdbcUrl (:jdbc-url config))
-                     (.setUsername (:username config))
-                     (.setPassword (:password config))
-                     (.setMaximumPoolSize (or (:maximum-pool-size config) 4))
-                     (.setMinimumIdle (or (:minimum-idle config) 2)))]
-    (->HikariAdapter datasource)))
+  (try
+    (let [datasource (doto (HikariDataSource.)
+                       (.setJdbcUrl (:jdbc-url config))
+                       (.setUsername (:username config))
+                       (.setPassword (:password config))
+                       (.setMaximumPoolSize (or (:maximum-pool-size config) 4))
+                       (.setMinimumIdle (or (:minimum-idle config) 2)))]
+      (->HikariAdapter datasource))
+    (catch Exception e
+      (throw (ex-info "Failed to create HikariAdapter"
+                      {:type ::adapter-creation-error
+                       :config config}
+                      e)))))
