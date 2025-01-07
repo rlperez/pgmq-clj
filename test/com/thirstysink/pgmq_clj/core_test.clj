d015d527c59c8f61b8c6139c622c532c61cc5ed1
Rigoberto L. Perez <rlperez@kablamo.me>
2025-01-07 01:08:23 -0500

    🧪 test: add tests for create and dropping a queue

--- test/com/thirstysink/pgmq_clj/core_test.clj
+++ test/com/thirstysink/pgmq_clj/core_test.clj
@@ -1,5 +1,5 @@
 (ns com.thirstysink.pgmq-clj.core-test
-  (:require [clojure.test :refer [deftest is use-fixtures]]
+  (:require [clojure.test :refer [deftest is use-fixtures testing]]
             [com.thirstysink.pgmq-clj.core :as core]
             [com.thirstysink.pgmq-clj.db.adapter :as adapter]
             [com.thirstysink.util.db :as db]))
@@ -18,11 +18,45 @@
 
     (core/create-queue adapter queue-name)
 
-    (let [result (adapter/query adapter "SELECT * FROM pgmq.list_queues() WHERE queue_name = ?;" queue-name)]
+    (let [result (adapter/query adapter "SELECT * FROM pgmq.list_queues() WHERE queue_name = ?;" [queue-name])]
       (is (= 1 (count result)))
       (is (= queue-name (:queue_name (first result)))))
 
     (core/drop-queue adapter queue-name)
 
-    (let [result (adapter/query adapter "SELECT * FROM pgmq.list_queues() WHERE queue_name = ?;" queue-name)]
+    (let [result (adapter/query adapter "SELECT * FROM pgmq.list_queues() WHERE queue_name = ?;" [queue-name])]
       (is (empty? result)))))
+
+(deftest create-queue-name-test
+  (testing "create-queue throws exception with an empty queue-name"
+    (is (thrown-with-msg?
+         clojure.lang.ExceptionInfo
+         #"Queue name must be a non-empty string"
+         (core/create-queue nil ""))))
+  (testing "create-queue throws exception with a nil queue-name"
+    (is (thrown-with-msg?
+         clojure.lang.ExceptionInfo
+         #"Queue name must be a non-empty string"
+         (core/create-queue nil nil))))
+  (testing "create-queue throws exception with a non string queue-name"
+    (is (thrown-with-msg?
+         clojure.lang.ExceptionInfo
+         #"Queue name must be a non-empty string"
+         (core/create-queue nil 1)))))
+
+(deftest drop-queue-name-test
+  (testing "drop-queue throws exception with an empty queue-name"
+    (is (thrown-with-msg?
+         clojure.lang.ExceptionInfo
+         #"Queue name must be a non-empty string"
+         (core/drop-queue nil ""))))
+  (testing "drop-queue throws exception with a nil queue-name"
+    (is (thrown-with-msg?
+         clojure.lang.ExceptionInfo
+         #"Queue name must be a non-empty string"
+         (core/drop-queue nil nil))))
+  (testing "drop-queue throws exception with a non string queue-name"
+    (is (thrown-with-msg?
+         clojure.lang.ExceptionInfo
+         #"Queue name must be a non-empty string"
+         (core/drop-queue nil 1)))))
