1748475be20ce9a100d6df51635327b003a292c8
Rigoberto L. Perez <rlperez@kablamo.me>
2025-01-06 21:36:33 -0500

    ✨ feat: add implementation to create and drop pgmq queues

--- src/com/thirstysink/pgmq_clj/core.clj
+++ src/com/thirstysink/pgmq_clj/core.clj
@@ -1,8 +1,19 @@
-(ns com.thirstysink.pgmq-clj.core)
+(ns com.thirstysink.pgmq-clj.core
+  (:require
+   [clojure.string :as str]
+   [com.thirstysink.pgmq-clj.db.adapter :as adapter]))
 
-(defn create-queue [adapter queue-name] nil)
+(defn create-queue [adapter queue-name]
+  (if (and (string? queue-name) (not (str/blank? queue-name)))
+    (let [create-sql "SELECT pgmq.create(?)"]
+      (adapter/execute! adapter create-sql [queue-name]))
+    (throw (ex-info "Queue name must be a non-empty string" {:value queue-name}))))
 
-(defn drop-queue [adapter queue-name] nil)
+(defn drop-queue [adapter queue-name]
+  (if (and (string? queue-name) (not (str/blank? queue-name)))
+    (let [create-sql "SELECT pgmq.drop_queue(?)"]
+      (adapter/execute! adapter create-sql [queue-name]))
+    (throw (ex-info "Queue name must be a non-empty string" {:value queue-name}))))
 
 (defn send-message [adapter queue-name payload] nil)
 
