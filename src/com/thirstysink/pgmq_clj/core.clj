4fba62ee679f73fb97bf4421421bf9c7dfb41f82
Rigoberto L. Perez <rlperez@kablamo.me>
2025-01-11 13:23:29 -0500

    🐛 fix: implement the read and send functions

--- src/com/thirstysink/pgmq_clj/core.clj
+++ src/com/thirstysink/pgmq_clj/core.clj
@@ -1,11 +1,42 @@
 (ns com.thirstysink.pgmq-clj.core
   (:require [clojure.spec.alpha :as s]
+            [cheshire.core :as ches]
             [com.thirstysink.pgmq-clj.db.adapter :as adapter]
-            [com.thirstysink.pgmq-clj.instrumentation :as inst]))
+            [com.thirstysink.pgmq-clj.instrumentation :as inst])
+  (:import (java.time.format DateTimeFormatter)))
 
 (s/def ::adapter #(satisfies? adapter/Adapter %))
 
 (s/def ::queue-name (s/and string? not-empty))
+
+(s/def ::visibility_time (s/and int? #(>= % 0)))
+
+(s/def ::quantity (s/and int? #(> % 0)))
+
+(s/def ::json map?)
+
+(s/def ::timestamp
+  (s/or :string (s/and string?
+                       #(try
+                          (.parse DateTimeFormatter/ISO_DATE_TIME %)
+                          true
+                          (catch Exception _ false)))
+        :instant #(instance? java.time.Instant %)))
+
+(s/def ::msg-id int?)
+
+(s/def ::read-ct int?)
+
+(s/def ::enqueued-at ::timestamp)
+
+(s/def ::vt ::timestamp)
+
+(s/def ::message string?)
+
+(s/def ::table-row
+  (s/keys :req-un [::msg-id ::read-ct ::enqueued-at ::vt ::message]))
+
+(s/def ::table-result (s/coll-of ::table-row))
 
 (s/fdef create-queue
   :args (s/cat :adapter ::adapter :queue-name ::queue-name)
@@ -21,12 +52,34 @@
 
 (defn drop-queue [adapter queue-name]
   (let [drop-sql "SELECT pgmq.drop_queue(?);"
-        result (adapter/execute! adapter drop-sql [queue-name])]
+        result (adapter/query adapter drop-sql [queue-name])]
     (get-in (first result) [:drop_queue])))
 
-(defn send-message [adapter queue-name payload] nil)
+(s/fdef send-message
+  :args (s/cat :adapter ::adapter
+               :queue-name ::queue-name
+               :payload ::json)
+  :ret int?)
 
-(defn read-message [adapter queue-name] nil)
+(defn send-message [adapter queue-name payload]
+  (let [json-payload (ches/generate-string payload)
+        send-sql "SELECT * from pgmq.send(?,?::jsonb);"
+        result (adapter/query adapter send-sql [queue-name json-payload])]
+    (get-in (first result) [:send])))
+
+(s/fdef read-message
+  :args (s/cat :adapter ::adapter
+               :queue-name ::queue-name
+               :visibility_time ::visibility_time
+               :quantity ::quantity
+               :filter ::json)
+  :ret ::table-result)
+
+(defn read-message [adapter queue-name visible_time quantity filter]
+  (let [json-filter (ches/generate-string filter)
+        read-sql "SELECT * FROM pgmq.read(?,?::integer,?::integer,?::jsonb);"
+        result (adapter/query adapter read-sql [queue-name visible_time quantity json-filter])]
+    result))
 
 (defn pop-message [adapter queue-name] nil)
 
