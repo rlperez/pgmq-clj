47f9beaaf1a44aadcba8e5af1be32a210f7531b9
Rigoberto L. Perez <rlperez@kablamo.me>
2025-01-29 21:34:48 -0500

    ♻️ refactor: move this stuff sort of needed for an adapter in the adapter

--- src/com/thirstysink/pgmq_clj/db/adapters/hikari_adapter.clj
+++ src/com/thirstysink/pgmq_clj/db/adapters/hikari_adapter.clj
@@ -1,8 +1,12 @@
 (ns com.thirstysink.pgmq-clj.db.adapters.hikari-adapter
   (:require [next.jdbc :as jdbc]
             [next.jdbc.result-set :as rs]
-            [com.thirstysink.pgmq-clj.db.adapter :as adapter])
-  (:import [com.zaxxer.hikari HikariDataSource]))
+            [next.jdbc.prepare :as prepare]
+            [com.thirstysink.pgmq-clj.db.adapter :as adapter]
+            [com.thirstysink.pgmq-clj.json :refer [<-json ->json]])
+  (:import [com.zaxxer.hikari HikariDataSource]
+           [org.postgresql.util PGobject]
+           [java.sql PreparedStatement]))
 
 (defrecord HikariAdapter [datasource]
   adapter/Adapter
@@ -57,6 +61,43 @@
         (throw (ex-info "Failed to close the datasource"
                         {:type ::close-error}
                         e))))))
+
+(defn ->pgobject
+  "Transforms Clojure data to a PGobject that contains the data as
+  JSON. PGObject type defaults to `jsonb` but can be changed via
+  metadata key `:pgtype`"
+  [x]
+  (let [pgtype (or (:pgtype (meta x)) "jsonb")]
+    (doto (PGobject.)
+      (.setType pgtype)
+      (.setValue (->json x)))))
+
+(defn <-pgobject
+  "Transform PGobject containing `json` or `jsonb` value to Clojure data."
+  [^PGobject v]
+  (let [type  (.getType v)
+        value (.getValue v)]
+    (if (#{"jsonb" "json"} type)
+      (some-> value <-json (with-meta {:pgtype type}))
+      value)))
+
+(extend-protocol prepare/SettableParameter
+  clojure.lang.IPersistentMap
+  (set-parameter [m ^PreparedStatement s i]
+    (.setObject s i (->pgobject m)))
+
+  clojure.lang.IPersistentVector
+  (set-parameter [v ^PreparedStatement s i]
+    (.setObject s i (->pgobject v))))
+
+;; if a row contains a PGobject then we'll convert them to Clojure data
+;; while reading (if column is either "json" or "jsonb" type):
+(extend-protocol rs/ReadableColumn
+  org.postgresql.util.PGobject
+  (read-column-by-label [^org.postgresql.util.PGobject v _]
+    (<-pgobject v))
+  (read-column-by-index [^org.postgresql.util.PGobject v _2 _3]
+    (<-pgobject v)))
 
 (defn ensure-pgmq-extension [adapter]
   (let [check-extension-sql "SELECT extname FROM pg_extension WHERE extname = 'pgmq';"
